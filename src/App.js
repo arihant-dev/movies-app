8fd9f1d5bbe148e21c1889f3f1849ab1028f18a6
Elia El Atram <eliaatram@outlook.com>
2026-05-13 18:42:35 +0200

    feat: add Public/Private routes + react context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import MovieDetailsPage from './pages/MovieDetailsPage'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+import PrivateRoute from './routes/PrivateRoute'
+import PublicRoute from './routes/PublicRoute'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/home" element={<HomePage />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/movie/:movieId" element={<MovieDetailsPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   )
 }
 
