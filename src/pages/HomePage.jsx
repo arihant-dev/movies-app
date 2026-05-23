b01ce87854f8236670f9703ae2ccc6a3b30a1f96
Elia El Atram <eliaatram@outlook.com>
2026-05-23 13:05:02 +0200

    feat: add HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import { movieApi } from '../constants/axios'
+import { movieRequests } from '../constants/requests'
+import Row from '../components/Row'
 
 const HomePage = () => {
+  const [movies, setMovies] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const request = await movieApi.get(movieRequests.fetchAllMovies)
+        setMovies(request.data.movies)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, []);
+
   return (
-    <div>
-      Home
+    <div className='page' style={{backgroundColor: "#111", overflow: "hidden"}}>
+      <Navbar />
+      <Banner />
+
+      {Object.keys(movies).map((category) => (
+        <Row key={category} title={category} movies={movies[category]}/>
+      ))}
     </div>
   )
 }
